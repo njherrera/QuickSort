030d4454b208f77237d7d265ef0f9f4ebe3c08ef
njherrera <nathan.j.herrera@gmail.com>
2022-01-13 17:30:04 -0800

    small commit because i have other stuff to do today, just wrote a brief outline of how quicksort works

---
+++ QuickSort.py
@@ -0,0 +1,4 @@
+# divide and conquer algorithm
+# selects pivot element from array and partitions elements according to whether they are greater or less than the pivot element
+# sub-arrays are then sorted recursively
+
